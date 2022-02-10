d72e3ce0d562248e2b3834c95f45d5d9b7e82d7d
wilvanos zhira <lex540@live.com>
2022-02-10 10:36:30 +0200

    lol

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const dotenv = require("dotenv")
+const colors = require('colors');
+const morgan = require( 'morgan');
+const passport = require('passport');
+const connectDB = require('./config/db')
+var cors = require('cors');
+var helmet = require('helmet')
+
+
+
+dotenv.config({path: './config/config.env'});
+
+
+
+connectDB();
+const app = express();
+app.use(passport.initialize());
+app.use(cors());
+app.use(helmet());
+app.use(passport.session());
+app.use(express.json());
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Serverr Runninng in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold)
+})
